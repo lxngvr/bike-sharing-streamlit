4d73843620ad5cd0652d8ca3803dcb0d1dedea68
Galang D. Ramadhan <129357205+lxngvr@users.noreply.github.com>
2026-03-29 21:12:27 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -85,7 +85,8 @@ elif menu == "RFM Analysis":
         'Monetary_Total_Volume': [day_df['casual'].sum(), day_df['registered'].sum()]
     }
     rfm_df = pd.DataFrame(rfm_data)
-    st.dataframe(rfm_df, use_container_width=True)
+    #st.dataframe(rfm_df, use_container_width=True)
+    st.markdown(rfm_df.to_markdown(index=False))
     
     st.subheader("Visualisasi Metrik RFM")
     fig, axes = plt.subplots(1, 3, figsize=(18, 6))
