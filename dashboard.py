16521874d41a970c87bbbdd5bb8b78dfe1d6d5f2
Galang D. Ramadhan <129357205+lxngvr@users.noreply.github.com>
2026-03-29 21:13:28 +0700

    Refactor dashboard.py to improve structure and clarity

--- dashboard.py
+++ dashboard.py
@@ -85,8 +85,7 @@ elif menu == "RFM Analysis":
         'Monetary_Total_Volume': [day_df['casual'].sum(), day_df['registered'].sum()]
     }
     rfm_df = pd.DataFrame(rfm_data)
-    #st.dataframe(rfm_df, use_container_width=True)
-    st.markdown(rfm_df.to_markdown(index=False))
+    st.dataframe(rfm_df, use_container_width=True)
     
     st.subheader("Visualisasi Metrik RFM")
     fig, axes = plt.subplots(1, 3, figsize=(18, 6))
